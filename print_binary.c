812cd5924ec1c295c5e53d716b04056c78b828b9
abdelhay <abdelhay@gmail.com>
2023-07-22 08:43:47 -0700

    code done, betty not yetgit add .

--- print_binary.c
+++ print_binary.c
@@ -3,12 +3,27 @@
 int printf_binary(va_list list)
 {
     unsigned int num = va_arg(list, unsigned int);
-    int binary_digits[32];
+    int binary_digits[sizeof(unsigned int) * 8];
     int i, j;
-    // convert the number to binary
-    for (i = 0; i < 32; i++)
+
+    // Convert the number to binary
+    for (i = 0; i < sizeof(unsigned int) * 8; i++)
     {
         binary_digits[i] = num & 1;
-        num >>=1
+        num >>= 1;
     }
+
+    // Skip leading zero
+    j = sizeof(unsigned int) * 8 - 1;
+    while (j >= 0 && binary_digits[j] == 0)
+    {
+        j--;
+    }
+    
+    // print the binary representation
+    for (; j >= 0; j--)
+    {
+        _putchar(binary_digits[j] + '0');
+    }
+    return (i);
 }
