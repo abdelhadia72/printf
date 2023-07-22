dc4215bf5468aca0f40f7125e204ca24ac1fa760
abdelhay <abdelhay@gmail.com>
2023-07-22 08:47:23 -0700

    betty try to fix

--- print_binary.c
+++ print_binary.c
@@ -1,29 +1,37 @@
 #include "main.h"
 
+/**
+ * printf_binary - Converts an unsigned integer to binary
+ *
+ * @list: The va_list containing the unsigned int argument.
+ *
+ * Return: The number of binary digits printed.
+ */
+
 int printf_binary(va_list list)
 {
-    unsigned int num = va_arg(list, unsigned int);
-    int binary_digits[sizeof(unsigned int) * 8];
-    int i, j;
+	unsigned int num = va_arg(list, unsigned int);
+	int binary_digits[sizeof(unsigned int) * 8];
+	int i, j;
 
-    // Convert the number to binary
-    for (i = 0; i < sizeof(unsigned int) * 8; i++)
-    {
-        binary_digits[i] = num & 1;
-        num >>= 1;
-    }
+	// Convert the number to binary
+	for (i = 0; i < sizeof(unsigned int) * 8; i++)
+	{
+		binary_digits[i] = num & 1;
+		num >>= 1;
+	}
 
-    // Skip leading zero
-    j = sizeof(unsigned int) * 8 - 1;
-    while (j >= 0 && binary_digits[j] == 0)
-    {
-        j--;
-    }
-    
-    // print the binary representation
-    for (; j >= 0; j--)
-    {
-        _putchar(binary_digits[j] + '0');
-    }
-    return (i);
+	// Skip leading zero
+	j = sizeof(unsigned int) * 8 - 1;
+	while (j >= 0 && binary_digits[j] == 0)
+	{
+		j--;
+	}
+	
+	// print the binary representation
+	for (; j >= 0; j--)
+	{
+		_putchar(binary_digits[j] + '0');
+	}
+	return (i);
 }
