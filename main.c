5e86f861856ce76253392932b9e114af09c4f8d5
abdelhay <abdelhay@gmail.com>
2023-07-23 12:07:59 -0700

    try to fixed benary

--- main.c
+++ main.c
@@ -1,60 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-#include <limits.h>
-#include <assert.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-	int len_1, len_2;
-
-	/* ===========> %o <=========== */
-	printf("----------------------\n");
-	printf("----> OCTAL CASES <---\n");
-	printf("----------------------\n");
-	len_1 = printf("%o\n", 0);
-	len_2 = _printf("%o\n", 0);
-	assert(len_1 == len_2);
-
-	len_2 = printf("%o\n", 255);
-	len_1 = _printf("%o\n", 255);
-	assert(len_1 == len_2);
-
-	len_1 = printf("%o\n", UINT_MAX);
-	len_2 = _printf("%o\n", UINT_MAX);
-	assert(len_1 == len_2);
-
-	/* ===========> %x <=========== */
-	printf("----------------------\n");
-	printf("-----> hex CASES <----\n");
-	printf("----------------------\n");
-	len_1 = printf("%x\n", 0);
-	len_2 = _printf("%x\n", 0);
-	assert(len_1 == len_2);
-
-	len_2 = printf("%x\n", 255);
-	len_1 = _printf("%x\n", 255);
-	assert(len_1 == len_2);
-
-	len_1 = printf("%x\n", UINT_MAX);
-	len_2 = _printf("%x\n", UINT_MAX);
-	assert(len_1 == len_2);
-
-	/* ===========> %X <=========== */
-	printf("----------------------\n");
-	printf("-----> HEX CASES <----\n");
-	printf("----------------------\n");
-	len_1 = printf("%X\n", 0);
-	len_2 = _printf("%X\n", 0);
-	assert(len_1 == len_2);
-
-	len_2 = printf("%X\n", 255);
-	len_1 = _printf("%X\n", 255);
-	assert(len_1 == len_2);
-
-	len_1 = printf("%X\n", UINT_MAX);
-	len_2 = _printf("%X\n", UINT_MAX);
-	assert(len_1 == len_2);
-
-
-	return (0);
+    _printf("%b\n", 98);
+    return (0);
 }
