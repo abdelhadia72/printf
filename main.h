741af271f37523e83b504257afc2b89b7cd12a32
abdelhay <abdelhay@gmail.com>
2023-07-23 10:00:05 -0700

    bianry should work now?

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdint.h>
 
 
 /* main funtion*/
--- print_binary.c
+++ print_binary.c
@@ -10,23 +10,27 @@
 
 int printf_binary(va_list list)
 {
-	unsigned int num = va_arg(list, unsigned int);
-	int binary_digits[sizeof(unsigned int) * 8];
-	int i, j;
+    unsigned int num = va_arg(list, unsigned int);
+    int binary_digits[sizeof(unsigned int) * 8];
+    size_t i, j;
 
-	for (i = 0; i < sizeof(unsigned int) * 8; i++)
-	{
-		binary_digits[i] = num & 1;
-		num >>= 1;
-	}
-	j = sizeof(unsigned int) * 8 - 1;
-	while (j >= 0 && binary_digits[j] == 0)
-	{
-		j--;
-	}
-	for (; j >= 0; j--)
-	{
-		_putchar(binary_digits[j] + '0');
-	}
-	return (i);
+    for (i = 0; i < sizeof(unsigned int) * 8; i++)
+    {
+        binary_digits[i] = num & 1;
+        num >>= 1;
+    }
+
+    j = sizeof(unsigned int) * 8 - 1;
+
+    while (j < SIZE_MAX && binary_digits[j] == 0)
+    {
+        j--;
+    }
+
+    for (; j < sizeof(unsigned int) * 8; j--)
+    {
+        _putchar(binary_digits[j] + '0');
+    }
+
+    return (i);
 }
