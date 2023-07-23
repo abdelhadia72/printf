b4f32a07b71221bbd9feb0745b6e3dbbd48271ca
abdelhadia72 <thisisavnb@gmail.com>
2023-07-23 18:49:19 +0100

    fixing int

--- print_int.c
+++ print_int.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <limits.h>
 /**
  * printf_decimal - Print a decimal number
  * @list: The va_list that contains the decimal number to print
@@ -9,36 +9,56 @@
 
 int printf_decimal(va_list list)
 {
-    int num = va_arg(list, int);
-    int num_len = 0;
-    int temp = num;
-    int divisor = 1;
-	int i; 
+	int num = va_arg(list, int);
+	int count = 0;
+	int is_negative = 0;
+	int num_digits;
+	int temp;
+	int divisor;
+	int i;
 
-    if (num < 0)
-    {
-        num = -num;
-        _putchar('-');
-        num_len++;
-    }
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    do
-    {
-        temp /= 10;
-        num_len++;
-    } while (temp != 0);
+	if (num == INT_MIN)
+	{
+		_putchar('-');
+		count++;
+		num = -(num + 1);
+	}
 
-    for (i = 1; i < num_len; i++)
-    {
-        divisor *= 10;
-    }
+	if (num < 0)
+	{
+		is_negative = 1;
+		num = -num;
+		_putchar('-');
+		count++;
+	}
 
-    while (divisor != 0)
-    {
-        int digit = num / divisor;
-        _putchar(digit + '0');
-        num %= divisor;
-        divisor /= 10;
-    }
-    return num_len;
+	num_digits = 0;
+	temp = num; 
+	while (temp)
+	{
+		temp /= 10;
+		num_digits++;
+	}
+
+	divisor = 1;
+
+	for (i = 1; i < num_digits; i++)
+		divisor *= 10;
+
+	while (divisor)
+	{
+		int digit = num / divisor;
+
+		_putchar('0' + digit);
+		num %= divisor;
+		divisor /= 10;
+		count++;
+	}
+	return (count + is_negative);
 }
