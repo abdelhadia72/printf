af52d8a49e260dd51c09a9979874fe0fee200d8d
abdelhay <abdelhay@gmail.com>
2023-07-23 05:02:10 -0700

    edit the main.h

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
+
 
 /* main funtion*/
 int _printf(const char *format, ...);
@@ -21,6 +21,11 @@ int printf_percent(va_list list);
 int printf_string(va_list list);
 int printf_char(va_list list);
 int printf_decimal(va_list list);
+int printf_binary(va_list list);
+int printf_x(va_list list);
+int printf_u(va_list list);
+int printf_o(va_list list);
+int printf_X(va_list list);
 
 /* helper funtions */
 int _putchar(char c);
