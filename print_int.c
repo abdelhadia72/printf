6bdbe2d78397d335f0a1228b7e532352bb7f6555
abdelhadia72 <thisisavnb@gmail.com>
2023-07-23 19:50:30 +0100

    fixing int

--- print_int.c
+++ print_int.c
@@ -1,64 +1,28 @@
 #include "main.h"
-#include <limits.h>
-/**
- * printf_decimal - Print a decimal number
- * @list: The va_list that contains the decimal number to print
- *
- * Return: Number of characters printed
- */
 
-int printf_decimal(va_list list)
-{
-	int num = va_arg(list, int);
+int print_int(int n) {
 	int count = 0;
-	int is_negative = 0;
-	int num_digits;
-	int temp;
-	int divisor;
-	int i;
+	unsigned int num;
 
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	if (num == INT_MIN)
-	{
+	if (n < 0) {
 		_putchar('-');
 		count++;
-		num = -(num + 1);
+		num = -n;
+	} else {
+		num = n;
 	}
 
-	if (num < 0)
-	{
-		is_negative = 1;
-		num = -num;
-		_putchar('-');
-		count++;
+	if (num / 10) {
+		count += print_int(num / 10);
 	}
 
-	num_digits = 0;
-	temp = num; 
-	while (temp)
-	{
-		temp /= 10;
-		num_digits++;
-	}
+	_putchar((num % 10) + '0');
+	count++;
 
-	divisor = 1;
+	return count;
+}
 
-	for (i = 1; i < num_digits; i++)
-		divisor *= 10;
-
-	while (divisor)
-	{
-		int digit = num / divisor;
-
-		_putchar('0' + digit);
-		num %= divisor;
-		divisor /= 10;
-		count++;
-	}
-	return (count + is_negative);
+int printf_decimal(va_list list) {
+	int value = va_arg(list, int);
+	return print_int(value);
 }
