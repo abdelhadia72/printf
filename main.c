5a5132412de8e8110f95a3c71d0f22c889588160
abdelhay <abdelhay@gmail.com>
2023-07-23 11:56:10 -0700

    fix binary?

--- main.c
+++ main.c
@@ -2,38 +2,57 @@
 #include <stdio.h>
 #include <limits.h>
 #include <assert.h>
+
 int main()
 {
-	int len_1 = 0, len_2 = 0;
+	int len_1, len_2;
 
-	/* ===========> %d <=========== */
+	/* ===========> %o <=========== */
 	printf("----------------------\n");
+	printf("----> OCTAL CASES <---\n");
 	printf("----------------------\n");
-	len_1 = printf("%d\n", 0);
-	len_2 = _printf("%d\n", 0);
+	len_1 = printf("%o\n", 0);
+	len_2 = _printf("%o\n", 0);
 	assert(len_1 == len_2);
 
-	len_2 = printf("%d\n", INT_MIN);
-	len_1 = _printf("%d\n", INT_MIN);
+	len_2 = printf("%o\n", 255);
+	len_1 = _printf("%o\n", 255);
 	assert(len_1 == len_2);
 
-	len_1 = printf("%d\n", INT_MAX);
-	len_2 = _printf("%d\n", INT_MAX);
+	len_1 = printf("%o\n", UINT_MAX);
+	len_2 = _printf("%o\n", UINT_MAX);
 	assert(len_1 == len_2);
 
-	/* ===========> %i <=========== */
+	/* ===========> %x <=========== */
 	printf("----------------------\n");
+	printf("-----> hex CASES <----\n");
 	printf("----------------------\n");
-	len_1 = printf("%i\n", 0);
-	len_2 = _printf("%i\n", 0);
+	len_1 = printf("%x\n", 0);
+	len_2 = _printf("%x\n", 0);
 	assert(len_1 == len_2);
 
-	len_2 = printf("%i\n", INT_MIN);
-	len_1 = _printf("%i\n", INT_MIN);
+	len_2 = printf("%x\n", 255);
+	len_1 = _printf("%x\n", 255);
 	assert(len_1 == len_2);
 
-	len_1 = printf("%i\n", INT_MAX);
-	len_2 = _printf("%i\n", INT_MAX);
+	len_1 = printf("%x\n", UINT_MAX);
+	len_2 = _printf("%x\n", UINT_MAX);
+	assert(len_1 == len_2);
+
+	/* ===========> %X <=========== */
+	printf("----------------------\n");
+	printf("-----> HEX CASES <----\n");
+	printf("----------------------\n");
+	len_1 = printf("%X\n", 0);
+	len_2 = _printf("%X\n", 0);
+	assert(len_1 == len_2);
+
+	len_2 = printf("%X\n", 255);
+	len_1 = _printf("%X\n", 255);
+	assert(len_1 == len_2);
+
+	len_1 = printf("%X\n", UINT_MAX);
+	len_2 = _printf("%X\n", UINT_MAX);
 	assert(len_1 == len_2);
 
 
--- print_binary.c
+++ print_binary.c
@@ -10,27 +10,24 @@
 
 int printf_binary(va_list list)
 {
-    unsigned int num = va_arg(list, unsigned int);
-    int binary_digits[sizeof(unsigned int) * 8];
-    size_t i, j;
+	unsigned int num = va_arg(list, unsigned int);
+	int binary_digits[sizeof(unsigned int) * 8];
+	size_t i, j;
 
-    for (i = 0; i < sizeof(unsigned int) * 8; i++)
-    {
-        binary_digits[i] = num & 1;
-        num >>= 1;
-    }
+	for (i = 0; i < sizeof(unsigned int) * 8; i++)
+	{
+		binary_digits[i] = num & 1;
+		num >>= 1;
+	}
+	j = sizeof(unsigned int) * 8 - 1;
 
-    j = sizeof(unsigned int) * 8 - 1;
-
-    while (j < SIZE_MAX && binary_digits[j] == 0)
-    {
-        j--;
-    }
-
-    for (; j < sizeof(unsigned int) * 8; j--)
-    {
-        _putchar(binary_digits[j] + '0');
-    }
-
-    return (i);
+	while (j < SIZE_MAX && binary_digits[j] == 0)
+	{
+		j--;
+	}
+	for (; j < sizeof(unsigned int) * 8; j--)
+	{
+		_putchar(binary_digits[j] + '0');
+	}
+	return (i);
 }
