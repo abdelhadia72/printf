482f45ee86f3f5a6fcadbbb2e655a576917338f7
abdelhay <abdelhay@gmail.com>
2023-07-23 14:36:50 -0700

    fixed binary

--- print_binary.c
+++ print_binary.c
@@ -9,32 +9,32 @@
  */
 int printf_binary(va_list list)
 {
-    unsigned int num = va_arg(list, unsigned int);
-    unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
-    int pr_chars = 0;
-    int leading_zeros = 1;
+	unsigned int num = va_arg(list, unsigned int);
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+	int pr_chars = 0;
+	int leading_zeros = 0;
 
-    if (num == 0)
-    {
-        _putchar('0');
-        return 1;
-    }
+	if (num == 0)
+	{
+		_putchar('0');
+		return 1;
+	}
 
-    while (mask)
-    {
-        if (num & mask)
-        {
-            _putchar('1');
-            leading_zeros = 0;
-        }
-        else if (!leading_zeros)
-        {
-            _putchar('0');
-        }
+	while (mask)
+	{
+		if (num & mask)
+		{
+			_putchar('1');
+			leading_zeros = 1;
+		}
+		else if (leading_zeros || mask == 1)
+		{
+			_putchar('0');
+		}
 
-        pr_chars++;
-        mask >>= 1;
-    }
+		pr_chars++;
+		mask >>= 1;
+	}
 
-    return pr_chars;
+	return pr_chars;
 }
