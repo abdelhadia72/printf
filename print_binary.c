4443a8a123fc87aa65b06005351451da31a2e2f8
abdelhadia72 <thisisavnb@gmail.com>
2023-07-24 01:36:41 +0100

    fixing print binary

--- print_binary.c
+++ print_binary.c
@@ -2,39 +2,38 @@
 
 /**
  * printf_binary - Converts an unsigned integer to binary
- *
  * @list: The va_list containing the unsigned int argument.
- *
  * Return: The number of binary digits printed.
  */
+
 int printf_binary(va_list list)
 {
-	unsigned int num = va_arg(list, unsigned int);
-	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
-	int pr_chars = 0;
-	int leading_zeros = 0;
+    unsigned int num = va_arg(list, unsigned int);
+    unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+    int pr_chars = 0;
+    int leading_zeros = 1;
+    if (num == 0)
+    {
+        _putchar('0');
+        return (1);
+    }
 
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
+    while (mask)
+    {
+        if (num & mask)
+        {
+            _putchar('1');
+            leading_zeros = 0;
+            pr_chars++;
+        }
+        else if (!leading_zeros || mask == 1)
+        {
+            _putchar('0');
+            pr_chars++;
+        }
 
-	while (mask)
-	{
-		if (num & mask)
-		{
-			_putchar('1');
-			leading_zeros = 1;
-		}
-		else if (leading_zeros || mask == 1)
-		{
-			_putchar('0');
-		}
+        mask >>= 1;
+    }
 
-		pr_chars++;
-		mask >>= 1;
-	}
-
-	return (pr_chars);
+    return (pr_chars);
 }
